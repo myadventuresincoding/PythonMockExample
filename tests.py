f8f6ad38355af7b8707ccced5affa30342ca107e
myadventuresincoding <66219262+myadventuresincoding@users.noreply.github.com>
2020-12-10 23:04:53 -0600

    Add explanations to each mock example

--- tests.py
+++ tests.py
@@ -5,6 +5,8 @@ from mock import patch, Mock
 
 
 class SomeOtherClassTest(unittest.TestCase):
+
+    # To mock a method in a class to return a specific value use @patch.object.
     @patch.object(my_module.MyClass, 'my_method')
     def test_my_method_shouldReturnTrue_whenMyMethodReturnsSomeValue(self, mock_my_method):
         # setup
@@ -17,6 +19,10 @@ class SomeOtherClassTest(unittest.TestCase):
         # assert
         self.assertTrue(result)
 
+    # To mock a method in a class with @patch.object but return a different value each time it is called,
+    # use side_effect. Side effect allows you to define a custom method and have that method called each time
+    # your mock method is called. The value returned from this method will be used as the return value from
+    # your mock method.
     @patch('my_module.MyClass')
     def test_my_method_shouldCallMyClassMethodMyMethod_whenSomeOtherClassMethodIsCalled(self, mock_my_class):
         # setup
@@ -28,6 +34,7 @@ class SomeOtherClassTest(unittest.TestCase):
         # assert
         self.assertTrue(mock_my_class.called)
 
+    # To mock an entire class to test interactions with that class use @patch.
     @patch('my_module.MyClass')
     def test_my_method_shouldReturnTrue_whenSomeOtherClassMethodIsCalledAndAReturnValueIsSet(self, mock_my_class):
         # setup
@@ -41,6 +48,9 @@ class SomeOtherClassTest(unittest.TestCase):
         # assert
         self.assertTrue(result)
 
+    # To mock an entire class with @patch and still set the return value of a method in that class,
+    # grab the instance of the mock object’s return value and set the method’s return value on the instance.
+    # There is a section on the patch page explaining how to do this.
     @patch.object(my_module.MyClass, 'my_method')
     def test_my_method_shouldReturnMultipleValues_whenMyMethodReturnsSomeValue(self, mock_my_method):
         # setup
